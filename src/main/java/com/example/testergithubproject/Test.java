cc98532b63c9b55f243169bc87b4e3b173fe0812
mukeshkumar36 <42973769+mukeshkumar36@users.noreply.github.com>
2024-06-29 16:20:41 +0400

    Update Test.java

--- src/main/java/com/example/testergithubproject/Test.java
+++ src/main/java/com/example/testergithubproject/Test.java
@@ -3,7 +3,7 @@ package com.example.testergithubproject;
 public class Test {
 
         public static void main(String[] args) {
-            System.out.println("Hello World!");
+            System.out.println("Hello World!")
 
 
 
